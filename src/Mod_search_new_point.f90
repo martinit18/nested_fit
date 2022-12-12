faa90f00bfd97ffb18424472ee4e5f3bf3889f77
César Godinho <c.godinho@campus.fct.unl.pt>
2022-12-12 14:58:10 +0000

    Simple MPI implementation for number of tries parallelization.

--- src/Mod_search_new_point.f90
+++ src/Mod_search_new_point.f90
@@ -32,7 +32,7 @@ CONTAINS
     ! Select the search method
     IF (search_method.eq.'RANDOM_WALK') THEN
         CALL RANDOM_WALK(n,itry,min_live_like,live_like,live, &
-       live_like_new,live_new,icluster,ntries,too_many_tries,n_call_cluster)
+          live_like_new,live_new,icluster,ntries,too_many_tries,n_call_cluster)
     ELSE IF(search_method .EQ. 'UNIFORM') THEN
         CALL UNIFORM(n,itry,min_live_like,live_like,live, &
           live_like_new,live_new,icluster,ntries,too_many_tries,n_call_cluster)
