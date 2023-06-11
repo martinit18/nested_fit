d83a3ae2ec2a439efa1fbd2788198483e8c68948
César Godinho <c.godinho@campus.fct.unl.pt>
2023-06-11 10:35:37 +0100

    Add optional ntries.

--- src/Mod_options.f90
+++ src/Mod_options.f90
@@ -4,5 +4,6 @@ MODULE MOD_OPTIONS
     IMPLICIT NONE
     LOGICAL :: opt_compact_output = .FALSE.
     CHARACTER(LEN=128) :: opt_input_file = 'nf_input.dat'
+    INTEGER :: opt_ntry = 0
     
 END MODULE MOD_OPTIONS
