81b9127b6f716a1d89d323d9579a0b23e3bfe94c
César Godinho <c.godinho@campus.fct.unl.pt>
2023-08-23 09:10:56 +0100

    Add error handling.

--- src/functions/internal_funcs.f90
+++ src/functions/internal_funcs.f90
@@ -1,4 +1,4 @@
-! Brief : This module contains the internal functions that get compiled to aid in the user-defined functions
+! Brief : This file contains the internal functions that get compiled to aid in the user-defined functions
 !         This gets compiled inside the shared library with the user functions
 !         There is no need to link against this with the main nested_fit executable
 ! Author : César Godinho
@@ -20,18 +20,15 @@ END FUNCTION
 
 FUNCTION Interpolate(interpolator_file, x, s)
     USE MOD_INTERPOLATE
-    IMPLICIT NONE    
-    CHARACTER(128), INTENT(IN) :: interpolator_file
-    REAL(8),        INTENT(IN) :: x
-    REAL(8),        INTENT(IN) :: s
-    REAL(8)                    :: Interpolate
-    TYPE(SplineData_t)         :: spline_data
-    LOGICAL                    :: error
-    INTEGER                    :: ierr
+    IMPLICIT NONE
+    CHARACTER(*), INTENT(IN) :: interpolator_file
+    REAL(8),      INTENT(IN) :: x
+    REAL(4),      INTENT(IN) :: s
+    REAL(8)                  :: Interpolate
+    TYPE(SplineData_t)       :: spline_data
+    LOGICAL                  :: error
 
     CALL GlobalSplineMap%find(interpolator_file, spline_data, error)
-
-    ! TODO(César) : There must be a more efficient way of doing this that I am not seeing now
     IF(.NOT.error) THEN
         ! Use spline data
         CALL EVALUATE_SPLINE_DATA(spline_data, x, Interpolate)
@@ -39,15 +36,26 @@ FUNCTION Interpolate(interpolator_file, x, s)
     ENDIF
     
     ! Interpolate the new spline data
-    CALL INTERPOLATE_FROM_FILE(interpolator_file, spline_data, s)
+    CALL INTERPOLATE_FROM_FILE(interpolator_file, spline_data, REAL(s, kind=8))
     CALL GlobalSplineMap%insert(interpolator_file, spline_data)
 
     CALL GlobalSplineMap%find(interpolator_file, spline_data, error)
-    IF(error) THEN
-        ! Fatal error
-        ! TODO(César) : How do we handle this if we are inside and OpenMP context???
-    ELSE
+    IF(.NOT.error) THEN
         CALL EVALUATE_SPLINE_DATA(spline_data, x, Interpolate)
         RETURN
+    ELSE
+        ! Fatal error
+        WRITE(*,*) '------------------------------------------------------------------------------------------------------------------'
+        WRITE(*,*) '       ERROR:           Failed to find the interpolation function in hashtable.'
+        WRITE(*,*) '       ERROR:           This is possibly a bug in the source code.'
+        WRITE(*,*) '       ERROR:           Please open an issue, reporting all your steps, so we are able to reproduce.'
+        WRITE(*,*) '       ERROR:           Aborting Execution...'
+        WRITE(*,*) '------------------------------------------------------------------------------------------------------------------'
+
+        ! TODO(César) : How do we handle this if we are inside an OpenMPI context???
+! #ifdef OPENMPI_ON
+!         CALL MPI_Abort(MPI_COMM_WORLD, 1, mpi_ierror)
+! #endif
+        STOP
     ENDIF
 END FUNCTION
