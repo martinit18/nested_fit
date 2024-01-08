3eefec341a84d912558fa79f89138bf3fe1321a2
César Godinho <cesarsky5@gmail.com>
2024-01-08 11:12:08 +0000

    Create separate module for integer stack. Increase size to 1024.

--- src/Mod_input_parse.f90
+++ src/Mod_input_parse.f90
@@ -7,6 +7,9 @@ MODULE MOD_INPUTPARSE
     USE MOD_MATH
     ! Logging module
     USE MOD_LOGGER
+    ! Integer stack module
+    USE MOD_INTSTACK
+
     IMPLICIT NONE
     PUBLIC :: InputDataMap_t, InputDataGenericValue_t, PARSE_INPUT
     PRIVATE
@@ -42,65 +45,7 @@ MODULE MOD_INPUTPARSE
         PROCEDURE, PUBLIC :: subkeys_of => INPUTDATA_MAP_SUBKEYSOF
     END TYPE InputDataMap_t
 
-    TYPE :: IntegerStack_t
-        INTEGER, DIMENSION(32)        :: values
-        INTEGER                       :: index = 0
-
-        CONTAINS
-        PROCEDURE, PUBLIC :: push  => STACK_PUSH
-        PROCEDURE, PUBLIC :: pop   => STACK_POP
-        PROCEDURE, PUBLIC :: top   => STACK_TOP
-        PROCEDURE, PUBLIC :: empty => STACK_EMPTY
-        PROCEDURE, PUBLIC :: count => STACK_COUNT
-    END TYPE IntegerStack_t
-
     CONTAINS
-
-    SUBROUTINE STACK_PUSH(stack, input)
-        CLASS(IntegerStack_t), INTENT(INOUT) :: stack
-        INTEGER              , INTENT(IN)    :: input
-
-        IF(stack%index.LE.32) THEN
-            stack%index = stack%index + 1
-            stack%values(stack%index) = input
-        ENDIF
-    END SUBROUTINE
-
-    SUBROUTINE STACK_POP(stack)
-        CLASS(IntegerStack_t), INTENT(INOUT) :: stack
-
-        IF(stack%index.GT.0) THEN
-            stack%index = stack%index - 1
-        ENDIF
-    END SUBROUTINE
-
-    SUBROUTINE STACK_TOP(stack, output)
-        CLASS(IntegerStack_t), INTENT(INOUT) :: stack
-        INTEGER              , INTENT(OUT)   :: output
-
-        IF(stack%index.GT.0) THEN
-            output = stack%values(stack%index)
-        ENDIF
-    END SUBROUTINE
-
-    SUBROUTINE STACK_EMPTY(stack, output)
-        CLASS(IntegerStack_t), INTENT(INOUT) :: stack
-        LOGICAL              , INTENT(OUT)   :: output
-
-        IF(stack%index.GT.0) THEN
-            output = .FALSE.
-        ELSE
-            output = .TRUE.
-        ENDIF
-    END SUBROUTINE
-
-    SUBROUTINE STACK_COUNT(stack, output)
-        CLASS(IntegerStack_t), INTENT(INOUT) :: stack
-        INTEGER              , INTENT(OUT)   :: output
-
-        output = stack%index
-    END SUBROUTINE
-
 
     SUBROUTINE COUNT_LBLANKS(input, count)
         CHARACTER(512), INTENT(IN)  :: input
