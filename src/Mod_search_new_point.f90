c84c1eb020a3633de6bd5bb6fe3eb72af841228e
Lune Maillard <98545787+lunemaillard@users.noreply.github.com>
2024-04-08 20:20:39 +0200

    Slight correction in Mod_search_new_point.f90

--- src/Mod_search_new_point.f90
+++ src/Mod_search_new_point.f90
@@ -973,7 +973,7 @@ SUBROUTINE SLICE_SAMPLING(n,itry,min_live_like,live_like,live, &
          DO m=1,npar
             IF(new_jump(m).LT.par_bnd1(m) .OR. new_jump(m).GT.par_bnd2(m)) THEN
                test_bnd=.FALSE.
-               WRITE(*,*) 'Parameter n° ', m, ' has a value outside its bounds'
+               IF(par_fix(m)==1) WRITE(*,*) 'Parameter n° ', m, ' has a value outside its bounds'
                EXIT
             ENDIF
          END DO !check if the new point is inside the sampled space
