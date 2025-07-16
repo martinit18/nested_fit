86654cb8bee283efd05e52f6bc722c7712bc3eb7
César Godinho <cesarsky5@gmail.com>
2025-07-16 11:03:25 +0100

    Add cli wrapper for nested_fit executable.

---
+++ pynested_fit/cliw.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+# Brief  : CLI wrapper for nested_fit executable (when installed via pip)
+# Author : César Godinho
+# Date   : 16/07/2025
+
+import os
+import sys
+import subprocess
+from .metadata import __version__
+
+def main():
+    exe = os.path.join(os.path.dirname(__file__), f'nested_fit{__version__}')
+    sys.exit(subprocess.call([exe] + sys.argv[1:]))
